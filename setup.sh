8e8540e6582578227da688b47e8b7486a3e421b8
mj1687 <alessandro.pasini12@gmail.com>
2017-11-24 19:40:53 +0100

    initial
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Script to setup an android build environment on Ubuntu 16.04 and above and Linux Mint 18
+
+clear
+echo Installing necessary libs!
+sudo apt update -y
+sudo apt install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
+automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev libssl-dev \
+libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
+liblzma* w3m adb fastboot maven ncftp htop imagemagick -y
+sudo apt-get install openjdk-8-jdk
+echo Dependencies have been installed
+echo repo has been Downloaded!
+echo Setting up USB Ports
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo chmod 644   /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo service udev restart
+adb kill-server
+sudo killall adb
